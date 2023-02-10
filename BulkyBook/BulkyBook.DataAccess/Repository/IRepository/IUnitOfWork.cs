bc6dc042e2210adb864248a2457a9729f28eee3a
Euheni <eugene.vezhnavets@gmail>
2023-02-10 15:23:00 +0300

    Add filling to UnitOfWork

--- BulkyBook/BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook/BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.Repository.IRepository
 {
-	internal interface IUnitOfWork
+	public interface IUnitOfWork
 	{
+		ICategoryRepository Category { get; }
+
+		void Save();
 	}
 }
--- BulkyBook/BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook/BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.Repository
 {
-	internal class UnitOfWork
+	public class UnitOfWork : IUnitOfWork
 	{
+		private ApplicationDbContext _db;
+		public ICategoryRepository Category { get; private set; }
+		public UnitOfWork(ApplicationDbContext db)
+		{
+			_db = db;
+			Category = new CategoryRepository(_db);
+		}
+
+		public void Save()
+		{
+			_db.SaveChanges();
+		}
 	}
 }
