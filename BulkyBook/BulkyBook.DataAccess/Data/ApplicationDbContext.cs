c3bdda692c39f580196fc5acb89921239feb57ed
Euheni <eugene.vezhnavets@gmail>
2023-02-25 23:03:26 +0300

    Add scaffold identity

--- BulkyBook/BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook/BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using BulkyBook.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBook.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
